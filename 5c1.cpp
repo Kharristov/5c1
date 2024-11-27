429ce9faab2e50d45a96dba3cadd7e84ba6172e1
Kharristov <annawey486@gmail.com>
2024-11-27 18:27:37 +0700

    Добавьте файлы проекта.

---
+++ 5c1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int b; 
+    int N;
+    cin >> b >> N; 
+    string result = "";
+
+    // Если число равно 0
+    if (N == 0) {
+        cout << 0;
+        return 0;
+    }
+
+    while (N > 0) {
+        int remainder = N % b; // остаток от деления на основание
+        if (remainder < 10) {
+            result = char(remainder + '0') + result; // цифры 0-9
+        }
+        else {
+            result = char(remainder - 10 + 'a') + result; // буквы a-f для чисел больше 9
+        }
+        N /= b;      
+    }
+
+    cout << result;
+
+    return 0;
+}
